65192a7fb2f41052ee9c07d69a0decdf07100c1d
Jon-Dionson <jonrichard.dionson@my.jcu.edu.au>
2021-03-15 08:16:18 +1000

    created score generating and determining program

---
+++ prac_03/broken_score.py
@@ -0,0 +1,22 @@
+def main():
+    score = int(input("Enter Score: "))
+    print(determine_score(score))
+
+
+def determine_score(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def generate_score():
+    import random
+    score = random.randint(1, 100)
+    print(f"{score} is {determine_score(score)}")
+
+
+# main()
+generate_score()
