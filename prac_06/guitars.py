8714235c7acb7f4385663b64e0eef5c425f6cc0e
Jon-Dionson <jonrichard.dionson@my.jcu.edu.au>
2021-04-18 19:38:08 +1000

    Finish question

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -10,6 +10,7 @@ while name != "":
     print(f"{new_guitar} added.")
     name = input("Name: ")
 
+
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
