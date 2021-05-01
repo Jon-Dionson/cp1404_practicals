efe72bed589cdd6a84572cc822d3e1d66ee85ca8
Jon-Dionson <jonrichard.dionson@my.jcu.edu.au>
2021-05-01 10:59:34 +1000

    Add error checking in handle_increment function

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -12,14 +12,31 @@ class MilesConverterApp(App):
     output_km = StringProperty()
 
     def build(self):
+        """build application from kv file"""
         self.title = "miles to kilometers conversion tool"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def convert_miles_to_kilometers(self, miles):
+        """converts miles to kilometers"""
+        # print(output_km)
         output_km = miles*MILES_TO_KILOMETERS
-        print(output_km)
+        # convert_to_number goes here
         self.root.ids.output_label.text = str(output_km)
+
+    def handle_increment(self, text, change):
+        """changes the number by increments of one"""
+        # miles = int(text) + int(change)
+        miles = int(self.convert_to_number(text)) + int(change)
+        self.root.ids.miles.text = str(miles)
+
+    @staticmethod
+    def convert_to_number(text):
+        """changes invalid input to 0"""
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
 
 
 MilesConverterApp().run()
