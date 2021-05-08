360727da03c08ca210be75415746c49da2974291
Jon-Dionson <jonrichard.dionson@my.jcu.edu.au>
2021-05-08 20:50:54 +1000

    Add display_taxis function.

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -6,7 +6,8 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
-    bill = 0
+    current_taxi = " "
+    total_bill = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     print(MENU)
@@ -14,36 +15,48 @@ def main():
     while choice != "q":
         if choice == "c":
             print("Taxis available")
-            for i, taxi in enumerate(taxis):
-                print(f"{i} - {taxi}")
-            taxi_choice = get_valid_number("Choose taxi: ", taxis)
-            print(taxi_choice)
+            display_taxis(taxis)
+            current_taxi = get_valid_taxi("Choose taxi: ", taxis)
+            print(taxis[current_taxi])
+
         elif choice == "d":
-            print("drive taxi")
+            if current_taxi == " ":
+                print("You need to choose a taxi before you can drive")
+            else:
+                # get kilometers
+                # get fare
+                # add fare to total_bill
+                pass
+
         else:
             print("Invalid option")
-            print(f"Bill to date: {bill}")
+        print(f"Bill to date: ${total_bill:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
 
 
-def get_valid_number(prompt, taxis):
-    """get valid taxi number"""
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+def get_valid_taxi(prompt, taxis):
+    """get valid taxi"""
     finished = False
+    number = 0
     while not finished:
         try:
             number = int(input(prompt))
             while number < 0:
                 print("Number must be > 0")
                 number = int(input(prompt))
-            while number > len(taxis):
+            while number >= len(taxis):
                 print("Invalid taxi choice")
                 number = int(input(prompt))
             finished = True
         except ValueError:
             print("Invalid input; enter a valid number")
-        for taxi in taxis:
-            return taxi
+    return number
 
 
 main()
